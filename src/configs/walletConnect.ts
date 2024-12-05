ded37819b7e0583fc018a2fa901e054e1d8f5b0f
Jonathan diaz <jfdiaz@playvox.com>
2024-12-04 22:17:32 -0600

    Refector wallet connect config modal

--- src/configs/walletConnect.ts
+++ src/configs/walletConnect.ts
@@ -1,34 +1,20 @@
 import { createAppKit } from "@reown/appkit/react";
 import { polygon, polygonAmoy } from "@reown/appkit/networks";
 
-import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
-
-import { cookieStorage, createStorage } from "@wagmi/core";
-
 const DEFAULT_NETWORK = import.meta.env.VITE_DEFAULT_NETWORK || "polygon";
 const defaultNetwork = DEFAULT_NETWORK == "polygon" ? polygon : polygonAmoy;
 export const createWalletConnectModal = () => {
   const projectId =
     import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID || ("" as string);
-  const networks = [defaultNetwork];
 
   const metadata = {
-    name: "DigitalP2P",
+    name: "DigitalP2P Exchange",
     description: "DigitalP2P Defi Protocol",
     url: "https://digitalp2pbot.github.io", // origin must match your domain & subdomain
     icons: ["https://avatars.mywebsite.com/"],
   };
-  const wagmiAdapter = new WagmiAdapter({
-    storage: createStorage({
-      storage: cookieStorage,
-    }),
-    ssr: true,
-    networks,
-    projectId,
-  });
 
   createAppKit({
-    adapters: [wagmiAdapter],
     networks: [defaultNetwork],
     defaultNetwork: defaultNetwork,
     metadata,
