3da16489fa7ae7307247487c52cfb30da8119870
Jonathan diaz <jfdiaz@playvox.com>
2024-12-04 21:11:21 -0600

    Test wagmi configuration

--- src/configs/walletConnect.ts
+++ src/configs/walletConnect.ts
@@ -4,11 +4,14 @@ import { polygon, polygonAmoy } from "@reown/appkit/networks";
 
 import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
 
+import { cookieStorage, createStorage } from "@wagmi/core";
+
 const DEFAULT_NETWORK = import.meta.env.VITE_DEFAULT_NETWORK || "polygon";
 const defaultNetwork = DEFAULT_NETWORK == "polygon" ? polygon : polygonAmoy;
 export const createWalletConnectModal = () => {
   const projectId =
     import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID || ("" as string);
+  const networks = [defaultNetwork];
 
   const metadata = {
     name: "DigitalP2P Exchange",
@@ -16,8 +19,17 @@ export const createWalletConnectModal = () => {
     url: "https://digitalp2p.co", // origin must match your domain & subdomain
     icons: ["https://avatars.mywebsite.com/"],
   };
+  const wagmiAdapter = new WagmiAdapter({
+    storage: createStorage({
+      storage: cookieStorage,
+    }),
+    ssr: true,
+    networks,
+    projectId,
+  });
 
   createAppKit({
+    adapters: [wagmiAdapter],
     networks: [defaultNetwork],
     defaultNetwork: defaultNetwork,
     metadata,
