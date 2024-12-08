eaf32ea81ad75f8555ca241e70de8142e3651c9d
Jonathan diaz <jfdiaz@playvox.com>
2024-12-07 21:05:51 -0600

    Remove adapters to check mobile connection

--- src/configs/walletConnect.ts
+++ src/configs/walletConnect.ts
@@ -1,8 +1,6 @@
 import { createAppKit } from "@reown/appkit/react";
 import { polygon, polygonAmoy } from "@reown/appkit/networks";
 
-import { EthersAdapter } from "@reown/appkit-adapter-ethers";
-
 const DEFAULT_NETWORK = import.meta.env.VITE_DEFAULT_NETWORK || "polygon";
 const defaultNetwork = DEFAULT_NETWORK == "polygon" ? polygon : polygonAmoy;
 export const createWalletConnectModal = () => {
@@ -17,7 +15,6 @@ export const createWalletConnectModal = () => {
   };
 
   createAppKit({
-    adapters: [new EthersAdapter()],
     networks: [defaultNetwork],
     defaultNetwork: defaultNetwork,
     metadata,
