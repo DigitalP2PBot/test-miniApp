5f3930239a47d9a4a0a1aadf2f8450410de6e1b0
Jonathan diaz <jfdiaz@playvox.com>
2025-02-04 12:20:06 -0600

    try to fix error upload deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), basicSsl()],
   build: {
-    outDir: "docs", // ✅ GitHub Pages works better with "dist"
+    outDir: "./docs",
   },
-  base: "/digitalp2pbot.github.io/", // ✅ Must match GitHub Pages repo name
+  base: "./",
   server: {
     port: 5174,
   },
