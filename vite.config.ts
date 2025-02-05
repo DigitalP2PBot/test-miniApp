67c079c2c9ac04d8b534c777b079c0fd8c13798a
Jonathan diaz <jfdiaz@playvox.com>
2025-02-04 20:41:01 -0600

    test push github action test 30

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react(), basicSsl()],
   build: {
-    outDir: "./docs",
+    outDir: "docs",
     emptyOutDir: true,
   },
-  base: "./",
+  base: "/miniApp/",
   server: {
     port: 5174,
   },
