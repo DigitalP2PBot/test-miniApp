1019a377c2843dfedf917bb4e6cc1f224559138e
Cristian <krishenaosanta@hotmail.com>
2025-03-15 21:29:33 -0500

    refactor: prevent content overflow

--- src/components/organism/InfoLabel.tsx
+++ src/components/organism/InfoLabel.tsx
@@ -6,10 +6,12 @@ type  Props = {
 }
 
 const InfoLabel: React.FC<Props> = ({label, content}) => {
+    const contentLength = content?.length || 0;
+    const shortedContent = contentLength > 50 ? `${content?.substring(0, 25)}...${content?.substring(contentLength -25, contentLength)}`: undefined;
     return (
         <div className="text-customGrayText mt-0 mr-0 mb-4 ml-0 text-left">
             <h4>{label} </h4>
-            <span className="text-dimGray">{content}</span>
+            <span className="text-dimGray" title={shortedContent}>{shortedContent || content}</span>
         </div>
     )
 }
